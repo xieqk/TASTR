91e9b93e68ab369b6ba1d5222f91d999c4d40306
xieqk <xieqiaokang@gmail.com>
2020-07-14 10:25:43 +0800

    Update train_dukemtmcreid.sh

--- scripts/train_dukemtmcreid.sh
+++ scripts/train_dukemtmcreid.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 gpu='0,1'
+
+data_root="/gdata1/xieqk/reid-dataset"
 time_now=$(date "+%Y%m%d-%H%M%S")
 save_dir="./logs/dukemtmcreid"
 
-python train_img_s1.py -g ${gpu} --save-dir ${save_dir}/${time_now}/s1
+python train_img_s1.py -g ${gpu} --root ${data_root} --save-dir ${save_dir}/${time_now}/s1
 
 sleep 5
 
-python train_img_s2.py -g ${gpu} --resume ${save_dir}/${time_now}/s1/s1_checkpoint_final.pth.tar --save-dir ${save_dir}/${time_now}
+python train_img_s2.py -g ${gpu} --root ${data_root} --resume ${save_dir}/${time_now}/s1/s1_checkpoint_final.pth.tar --save-dir ${save_dir}/${time_now}
